df3073bbc859a980d2dea56901c8a0c76044f06f
Minhaj <minhaj.kk@gmail.com>
2015-05-18 01:56:55 +0400

    hipster stuff

---
+++ public/javascript/application.js
@@ -0,0 +1,25 @@
+/*************************************
+//
+// url-shortener app
+//
+**************************************/
+// connect to our socket server
+var socket = io('http://localhost:3000/');
+
+// shortcut for document.ready
+$(function(){
+    $("#flasher-div").css("display","none")
+
+	//setup some common vars
+	var $submitButton = $('#submitBtn'),
+        $urlTxt = $('#url'),
+        $flasher = $('#url-flasher');
+
+	//SOCKET STUFF
+	socket.on("url.changed", function(data){
+        $("#flasher-div").show();
+        $flasher.html("Just Created: " + data.url);
+        $flasher.removeClass('animated rubberBand');
+        setTimeout(function(){$flasher.addClass('animated rubberBand')}, 1);
+	});
+});
--- routes/index.js
+++ routes/index.js
@@ -1,65 +1,59 @@
-var express = require('express');
-var randomstring = require("randomstring");
-var router = express.Router();
-var Url = require('../models/url.js');
-router.get('/:slug', function(req, res){
-    //var query  = Url.where({ slug: req.params.slug });
-    Url.findOne({'slug': req.params.slug}, function(err, urlObj){
-        console.log('into mongoose findone');
-        console.log(urlObj)
-        if(err) {
-            console.log("Error occurred while searching url for slug:" + req.params.slug);
-            console.log("Error-> " + err);
-        }
+var randomstring = require("randomstring")
+    , Url = require('../models/url.js');
 
-        if(urlObj){
-            var _url2Redirect = urlObj.url;
-            console.log("Found Url-> " + _url2Redirect);
-            return res.render('redir', {redirect: _url2Redirect});
-        } else {
-            req.session.flash = {
-                type: 'danger',
-                intro: 'Uh oh,',
-                message: 'We couldn\'t find a link for the URL you clicked.'
-            };
-            res.redirect('/');
+module.exports = function(app, io, server) {
+
+    app.get('/:slug', function(req, res){
+        if (req.params.slug) {
+            Url.findOne({'slug': req.params.slug}, function(err, urlObj){
+                if(err) {
+                    console.log("Error occurred while searching url for slug:" + req.params.slug);
+                }
+
+                if(urlObj){
+                    var _url2Redirect = urlObj.url;
+                    return res.render('redir', {redirect: _url2Redirect});
+                } else {
+                    req.session.flash = {
+                        type: 'danger',
+                        intro: 'Uh oh,',
+                        message: 'We couldn\'t find a link for the URL you clicked.'
+                    };
+                    res.redirect('/');
+                }
+            });
         }
     });
-});
 
-/* GET home page. */
-router.get('/', function(req, res){
-    res.render('home');
-});
-
-router.post('/shortener',  function(req, res){
-    console.log("Called shortener");
-
-    var url = new Url({
-        url: req.body.url,
-        slug: randomstring.generate(5)
+    /* GET home page. */
+    app.get('/', function(req, res){
+        res.render('home');
     });
-    url.save(function (err, url) {
-        if (err) {
-            console.error(err);
-            req.session.flash = {
-                type: 'danger',
-                intro: 'Ooops!',
-                message: 'There was an error processing your request.'
-            };
-        } else {
-            req.session.flash = {
-                type: 'success',
-                intro: 'DONE!!',
-                message: 'YOU\'VE CREATED YOUR URL SHORTENER!',
-                slug: url.slug
-            };
-
-            console.log("Saved Url:" +  req.body.url);
-            console.log("Slug:" +  url.slug);
-        }
-        res.redirect(303, '/');
+    
+    /* Creating short url. */
+    app.post('/shortener',  function(req, res){ 
+        var url = new Url({
+            url: req.body.url,
+            slug: randomstring.generate(5)
+        });
+        url.save(function (err, url) {
+            if (err) {
+                req.session.flash = {
+                    type: 'danger',
+                    intro: 'Ooops!',
+                    message: 'There was an error processing your request.'
+                };
+            } else {
+                req.session.flash = {
+                    type: 'success',
+                    intro: 'DONE!!',
+                    message: 'YOU\'VE CREATED YOUR URL SHORTENER!',
+                    slug: url.slug
+                };
+                io.to('clients').emit("url.changed", {url: server+url.slug});
+            }
+            res.redirect(303, '/');
+        });
     });
-});
+};
 
-module.exports = router;
