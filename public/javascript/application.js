9603c8a65157d87caf384116ad779f5d515aff45
Minhaj <minhaj.kk@gmail.com>
2015-05-18 21:55:17 +0400

    changes here and there

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -4,22 +4,20 @@
 //
 **************************************/
 // connect to our socket server
-var socket = io('https://node-us.herokuapp.com/');
+var socket = io('http://localhost:3000/');
 
 // shortcut for document.ready
 $(function(){
-    $("#flasher-div").css("display","none")
+    $("#flasher-div").css("display","none");
 
 	//setup some common vars
-	var $submitButton = $('#submitBtn'),
-        $urlTxt = $('#url'),
-        $flasher = $('#url-flasher');
+	var $flasher = $('#url-flasher');
 
 	//SOCKET STUFF
 	socket.on("url.changed", function(data){
         $("#flasher-div").show();
-        $flasher.html("Just Created: " + data.url);
-        $flasher.removeClass('animated rubberBand');
-        setTimeout(function(){$flasher.addClass('animated rubberBand')}, 1);
+        $flasher.html("Newly shorten: " + data.url);
+        $flasher.removeClass('animated fadeInLeftBig');
+        setTimeout(function(){$flasher.addClass('animated fadeInLeftBig')}, 1);
 	});
 });
--- routes/index.js
+++ routes/index.js
@@ -50,7 +50,7 @@ module.exports = function(app, io, server) {
                     message: 'YOU\'VE CREATED YOUR URL SHORTENER!',
                     slug: url.slug
                 };
-                io.to('clients').emit("url.changed", {url: server+url.slug});
+                io.to('clients').emit("url.changed", {url: url.url});
             }
             res.redirect(303, '/');
         });
